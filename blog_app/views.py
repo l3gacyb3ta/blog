ed1a40b1f5c8d8cc46354b3fa61557467e5391bf
Some Dude <l3gacy.b3ta@gmail.com>
2021-01-15 20:43:33 -0500

    CC-by-sa license!

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from .models import Post, Contact, Project
 
 global footer
-footer = "Bam"
+footer = "<a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/4.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-sa/4.0/80x15.png\" /></a><br />This work is licensed under a <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/4.0/\">Creative Commons Attribution-ShareAlike 4.0 International License</a>."
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
