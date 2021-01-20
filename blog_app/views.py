f4d256e4c34c16c206b85f7f224fb995372e833d
L3gacy B3ta <58434499+l3gacyb3ta@users.noreply.github.com>
2021-01-20 18:19:22 -0500

    Fixed.

--- blog_app/views.py
+++ blog_app/views.py
@@ -6,7 +6,7 @@ global footer
 footer = "<a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/4.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-sa/4.0/80x15.png\" /></a><br />This work is licensed under a <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/4.0/\">Creative Commons Attribution-ShareAlike 4.0 International License</a>."
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date').reverse()
     return render(request, 'blog/post_list.html', {'posts': posts, 'footer': footer})
 
 def post_detail(request, pk):
@@ -21,5 +21,5 @@ def contact(request):
     return render(request, 'blog/contact.html', { 'contacts' : contacts, 'footer': footer})
 
 def projects(request):
-    projects = Project.objects.filter(created_date__lte=timezone.now()).order_by('created_date').reverse()
+    projects = Project.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
     return render(request, 'blog/projects.html', { 'projects': projects, 'footer': footer})
